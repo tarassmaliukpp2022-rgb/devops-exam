a334b79328e65f7ba4233b885ec336050446a14e
user <user@users-MacBook-Air.local>
2026-04-06 19:13:48 +0300

    fix: update backend.tf for DO Spaces compatibility

--- task1/terraform/backend.tf
+++ task1/terraform/backend.tf
@@ -8,10 +8,11 @@ terraform {
     }
   }
 
-  # tfstate зберігається у DigitalOcean Spaces (S3-сумісне сховище)
   backend "s3" {
-    endpoint = "https://fra1.digitaloceanspaces.com"
-    region   = "us-east-1" # фіктивне значення, обов'язкове для S3-backend
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    region = "us-east-1"
 
     bucket = "smaliuk-bucket"
     key    = "terraform/task1/terraform.tfstate"
@@ -19,6 +20,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 }
